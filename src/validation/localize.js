f877fa3e9b8f6b691097ef9c194d1e79dcc52d1a
jana <jana.c@systemli.org>
2025-03-09 16:53:59 +0100

    Improved comments

--- src/validation/localize.js
+++ src/validation/localize.js
@@ -1,5 +1,12 @@
-export default (message,...params) => {
+/**
+ * @description Creates a localization function for validation messages.
+ * @param {string} message - The message key to be localized.
+ * @param {...*} params - Additional parameters to be passed to the localization function.
+ * @returns {function} A middleware function that returns the localized message.
+ */
+export default (message, ...params) => {
     return (value, { req, location, path }) => {
-        return req.__(message,...params);
-    }
-}
+        // Return the localized message using the provided message key and parameters
+        return req.__(message, ...params);
+    };
+};
