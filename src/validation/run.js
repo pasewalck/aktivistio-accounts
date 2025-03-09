cffcc514a212abc02fa9295e8961903c66a9d248
Pasewalck <pasewalck@posteo.net>
2025-03-09 16:56:12 +0100

    Added comments

--- src/validation/run.js
+++ src/validation/run.js
@@ -1,5 +1,11 @@
-export default (req,checks) => {
+/**
+ * @description Executes a series of validation checks on the request.
+ * @param {import("express").Request} req - The Express request object.
+ * @param {Array} checks - An array of validation check objects that implement a `run` method.
+ */
+export default (req, checks) => {
+    // Iterate over each check and execute it with the request object
     checks.forEach(check => {
         check.run(req);
     });
-}
+};
