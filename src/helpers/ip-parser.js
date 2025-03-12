0d1fa73d9b1b165eab6d7d038b88ce8371c34f3a
Pasewalck <pasewalck@posteo.net>
2025-03-12 18:17:09 +0100

    Added simple ip parse function

---
+++ src/helpers/ip-parser.js
@@ -0,0 +1,23 @@
+import logger from './logger.js';
+
+/**
+ * @description Parses an IP Address from supplied request
+ * @param {import("express").Request} req - The request to parse
+ * @returns {String|null} The IP Address found or null if not found
+ */
+function parseIP(req) {
+    try {
+        // Check for x-forwarded-for header and split it to get the first IP
+        const ip = req.headers["x-forwarded-for"] 
+            ? req.headers["x-forwarded-for"].split(',')[0].trim() 
+            : req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+
+        if (!ip) {
+            logger.warn("Unable to obtain IP Address from request");
+        }
+        return ip || null; // Return null if no IP is found
+    } catch (error) {
+        logger.warn("Error while obtaining IP Address from request:", error);
+        return null; // Return null in case of an error
+    }
+}
