34fe508d83754f0f980a079c312892c01e324994
jana <jana.c@systemli.org>
2025-06-11 20:48:23 +0200

    Add audit action types

---
+++ src/models/audit-action-types.js
@@ -0,0 +1,10 @@
+/**
+ * @enum {String}
+ * Enum for action types in the audit log.
+ */
+export const AuditActionType = {
+    LOGIN: 'login',
+    PASSWORD_RECOVERY: 'password_recovery',
+    ACCOUNT_CREATION: 'account_creation',
+    ACCOUNT_DELETION: 'account_deletion',
+};
