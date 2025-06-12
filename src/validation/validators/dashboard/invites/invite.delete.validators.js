878b430fb0c44310a98eaaa72d376b8d4ad94c17
Pasewalck <pasewalck@posteo.net>
2025-06-12 15:49:06 +0200

    Fix invite delete validator

--- src/validation/validators/dashboard/invites/invite.delete.validators.js
+++ src/validation/validators/dashboard/invites/invite.delete.validators.js
@@ -4,7 +4,7 @@ import localize from "../../../localize.js";
 import invitesService from "../../../../services/invites.service.js";
 
 export default [
-    body("invite").exists({checkFalsy: true}).withMessage(localize('Invite code is not defined')).bail()
+    body("code").exists({checkFalsy: true}).withMessage(localize('Invite code is not defined')).bail()
         .escape()
         .isAlphanumeric().withMessage(localize('Invite code format is invalid')).bail()
         .custom((value) => (!!invitesService.validate(value))).withMessage(localize('Invite code is invalid')),
