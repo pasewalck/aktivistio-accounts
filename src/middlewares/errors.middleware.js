b27478f3a9416d54bf84597ae94c89bd65ca1ead
jana <jana.c@systemli.org>
2025-06-18 19:33:13 +0200

    Use new error model in middleware

--- src/middlewares/errors.middleware.js
+++ src/middlewares/errors.middleware.js
@@ -1,4 +1,5 @@
 import logger from "../helpers/logger.js";
+import { ClientError, InternalError } from "../models/errors.js";
 import sharedRenderer from "../renderers/shared.renderer.js";
 /**
  * @description Middleware for handling errors in the application.
@@ -9,8 +10,20 @@ import sharedRenderer from "../renderers/shared.renderer.js";
  * @param {import("express").NextFunction} next - The next middleware function.
  */
 const errorMiddleware = (err, req, res, next) => {
-    logger.error(err); // Log the error
-    sharedRenderer.error(res, err); // Render an error response
-};
 
+    switch (true) {
+        case err instanceof InternalError:
+            console.error(err); // Log the internal error for debugging
+            return sharedRenderer.error(res,res.__('An internal error occurred. Please try again later.'),err.statusCode); // Render an error response
+
+        case err instanceof ClientError || err.constructor.name === "ForbiddenError":
+            return sharedRenderer.error(res,res.__("An error occurred with the following message: %s",res.__(err.message)),err.statusCode); // Render an error response
+
+        default:
+            // Handle other types of errors not known
+            logger.error(err); // Log the error
+            return sharedRenderer.error(res,res.__('An unexpected error occurred.'),500); // Render an error response    
+    }
+
+};
 export default errorMiddleware;
