76db2209e5b903180eb5fe836c4d26a883c95173
jana <jana.c@systemli.org>
2025-06-18 19:31:50 +0200

    Add error model

---
+++ src/models/errors.js
@@ -0,0 +1,31 @@
+/**
+ * Represents an internal server error.
+ * @extends Error
+ */
+export class InternalError extends Error {
+    /**
+     * Creates an instance of InternalError.
+     * @param {string} message - The error message.
+     */
+    constructor(message) {
+        super(message);
+        this.name = "InternalError";
+        this.statusCode = 500; // Internal Server Error
+    }
+}
+
+/**
+ * Represents a client error.
+ * @extends Error
+ */
+export class ClientError extends Error {
+    /**
+     * Creates an instance of ClientError.
+     * @param {string} message - The error message.
+     */
+    constructor(message) {
+        super(message);
+        this.name = "ClientError";
+        this.statusCode = 400; // Bad Request
+    }
+}
