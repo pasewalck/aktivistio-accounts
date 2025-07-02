6ebc0c955938a9ed3ed7a66c9eebc08825a06291
Pasewalck <pasewalck@posteo.net>
2025-07-02 14:40:43 +0200

    Add account action types.

---
+++ src/models/account.action-token-types.js
@@ -0,0 +1,9 @@
+/**
+ * Enum for account action token types.
+ * @readonly
+ * @enum {string}
+ */
+export class AccountActionTokenTypes {
+    static ACCOUNT_SETUP = "account_setup";
+    static PASSWORD_RESET = "password_reset";
+}
