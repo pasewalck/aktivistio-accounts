4312bc4d886fa6a3eaa993caf72e820536485373
Pasewalck <pasewalck@posteo.net>
2025-07-30 17:14:51 +0200

    Update launcher

--- src/launcher.js
+++ src/launcher.js
@@ -2,13 +2,19 @@ import { createLauncher } from "init-secret-launcher";
 import { Secret } from "init-secret-launcher/secret.js";
 import { generateAlphanumericSecret } from "./helpers/generate-secrets.js";
 import spawn from "cross-spawn";
+import logger from "./helpers/logger.js";
 
 createLauncher([
     new Secret("DATABASE_KEY_DATA",() => generateAlphanumericSecret(40)),
     new Secret("DATABASE_KEY_OIDC",() => generateAlphanumericSecret(40)),
     new Secret("DATABASE_KEY_SECRETS",() => generateAlphanumericSecret(40)),
     new Secret("DATABASE_KEY_SESSIONS",() => generateAlphanumericSecret(40))
-],"database-secrets.txt",process.env.LAUNCHER_PORT | 3000,(secrets) => {
+],"database-secrets.txt",process.env.LAUNCHER_PORT | 3000,() => {
+    const password = generateAlphanumericSecret(40)
+    logger.info(`Launcher initiated with new password: ${password}`)
+    return password;
+},(secrets) => {
+    logger.info("Launcher unlocked and starting main service ...")
     const child = spawn('node', ['src/server.js'], {
         env: {
             ...process.env,
