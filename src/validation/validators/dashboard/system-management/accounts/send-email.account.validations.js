2b4a11e0214f0c9965362eedf36d9538ad62d72c
jana <jana.c@systemli.org>
2025-08-14 18:50:31 +0200

    Cleanup

--- src/validation/validators/dashboard/system-management/accounts/send-email.account.validations.js
+++ src/validation/validators/dashboard/system-management/accounts/send-email.account.validations.js
@@ -1,7 +1,5 @@
 import { body } from "express-validator";
 import localize from "../../../../localize.js";
-import { Role } from "../../../../../models/roles.js";
-import usernameCreateValidator from "../../../../util-validators/username-create.validator.js";
 
 export default [
     body('email').notEmpty().withMessage(localize('email.required')).bail().escape().isEmail().withMessage(localize('email.format.invalid'))
