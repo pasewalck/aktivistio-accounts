70481db5937fc46482e7259cb8b53f8911fae183
Pasewalck <pasewalck@posteo.net>
2025-08-14 14:15:50 +0200

    Remove type and format checking for username on login

--- src/validation/util-validators/username-exists.validator.js
+++ src/validation/util-validators/username-exists.validator.js
@@ -10,8 +10,6 @@ export default (validationChain) => {
         .exists({checkFalsy: true})
         .escape()
         .toLowerCase()
-        .isAlphanumeric().withMessage(localize('validation.username.requirements.alphanumeric')).bail()
-        .isLength({min: 2, max: 14}).withMessage(localize('validation.username.requirements.length')).bail()
         .custom((value) => (accountService.find.withUsername(value)))
             .withMessage(localize("validation.username.not-found"))
 } 
