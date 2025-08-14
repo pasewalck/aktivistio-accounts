828855abc4db87cc8ebc75a015df51282d87de1e
Pasewalck <pasewalck@posteo.net>
2025-08-14 13:48:34 +0200

    Improve and fixup translations

--- src/validation/util-validators/username-create.validator.js
+++ src/validation/util-validators/username-create.validator.js
@@ -10,7 +10,7 @@ export default (validationChain) => {
         .exists({checkFalsy: true}).bail()
         .isString()
         .escape()
-        .isLowercase().withMessage(localize("Usernames are not allowed to use upercase letters"))
+        .isLowercase().withMessage(localize("validation.username.only_lowercases"))
         .isAlphanumeric().withMessage(localize("validation.username.characters_allowed"))
         .isLength({ min: 2,max:14 }).withMessage(localize("validation.username.length"))
         .custom((value) => !(!!accountService.find.withUsername(value))).withMessage(localize("Username is taken"))
