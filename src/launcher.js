57c5f131fca8c12252144dd69a0db3951f80e3f5
pasewalck <pasewalck@posteo.net>
2026-03-27 10:17:03 +0100

    Improve launcher integration.

--- src/launcher.js
+++ src/launcher.js
@@ -5,6 +5,41 @@ import logger from './helpers/logger.js';
 import { extendUrl } from './helpers/url.js';
 
 logger.info(process.env.BASE_URL != undefined ? process.env.BASE_URL : '');
+let child;
+let secrets;
+let restarts = 0;
+let restart = true;
+
+function scheduleRestart() {
+	if (!restart) return;
+	restarts += 1;
+	const delay = 1000 * Math.min(2 ** restarts, 16);
+	logger.info(`Restarting main service in ${delay} ms (attempt ${restarts})`);
+	setTimeout(startChild, delay);
+}
+
+function startChild() {
+	logger.info('Starting main service ...');
+	child = spawn('node', ['src/server.js'], {
+		env: {
+			...process.env,
+			...secrets,
+		},
+		stdio: 'inherit',
+	});
+
+	logger.debug(`Started main service as child with pid=${child.pid}`);
+
+	child.on('exit', (code, signal) => {
+		logger.warn(`Main service exited pid=${child?.pid} code=${code} signal=${signal}`);
+		scheduleRestart();
+	});
+
+	child.on('error', (err) => {
+		logger.error('Failed to start main service as child:', err);
+		scheduleRestart();
+	});
+}
 
 createLauncher(
 	[
@@ -20,15 +55,9 @@ createLauncher(
 		logger.info(`Launcher initiated with new password: ${password}`);
 		return password;
 	},
-	(secrets) => {
-		logger.info('Starting main service ...');
-		spawn('node', ['src/server.js'], {
-			env: {
-				...process.env,
-				...secrets,
-			},
-			stdio: 'inherit',
-		});
+	(overwriteSecrets) => {
+		secrets = overwriteSecrets;
+		startChild();
 	},
 	() => {},
 	(isError, ...message) => {
@@ -37,4 +66,18 @@ createLauncher(
 	},
 	extendUrl(new URL(process.env.BASE_URL != undefined ? process.env.BASE_URL : 'http://localhost:3000'), 'health')
 		.href
+);
+
+['SIGINT', 'SIGTERM', 'SIGHUP'].forEach((sig) =>
+	process.on(sig, () => {
+		if (child && child.pid) {
+			restart = false;
+			try {
+				process.kill(child.pid, sig);
+			} catch {
+				logger.error('Failed to kill main process.');
+			}
+		}
+		process.exit();
+	})
 );
