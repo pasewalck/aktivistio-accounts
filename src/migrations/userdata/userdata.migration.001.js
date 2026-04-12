66d990b94bc123c2d26ddfebdbeab4725e45d15d
pasewalck <pasewalck@posteo.net>
2026-04-12 20:56:06 +0200

    feat: further work on invite rework (modify migration)

--- src/migrations/userdata/userdata.migration.001.js
+++ src/migrations/userdata/userdata.migration.001.js
@@ -4,11 +4,6 @@ import Migration from '../../models/migration.js';
 export default new MigrationVersion(1, [
 	new Migration((db) => {
 		db.exec(`
-			ALTER TABLE invites ADD COLUMN shared_invite INTEGER DEFAULT 0;
-		`);
-	}),
-	new Migration((db) => {
-		db.exec(`
 			ALTER TABLE invites ADD COLUMN last_use INTEGER;
 			ALTER TABLE invites ADD COLUMN max_uses INTEGER DEFAULT 1;
 			ALTER TABLE invites ADD COLUMN uses INTEGER DEFAULT 0;
@@ -18,6 +13,11 @@ export default new MigrationVersion(1, [
 	new Migration((db) => {
 		db.exec(`
 			ALTER TABLE invites DROP COLUMN usages;
+		`);
+	}),
+	new Migration((db) => {
+		db.exec(`
+			ALTER TABLE invites ADD COLUMN system_invite INTEGER DEFAULT 0;
 		`);
 	}),
 ]);
