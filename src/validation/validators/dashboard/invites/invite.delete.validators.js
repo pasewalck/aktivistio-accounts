290aee76300c539496a73cc33405f767c62f8cdd
pasewalck <pasewalck@posteo.net>
2026-04-13 11:47:46 +0200

    fix(exploit): fix exploit being able to check and any delete invites with unlimited multiplier role

--- src/validation/validators/dashboard/invites/invite.delete.validators.js
+++ src/validation/validators/dashboard/invites/invite.delete.validators.js
@@ -2,6 +2,7 @@ import { body } from 'express-validator';
 
 import localize from '../../../localize.js';
 import invitesService from '../../../../services/invites.service.js';
+import { hasPermission, Permission } from '../../../../models/roles.js';
 
 export default [
 	body('code')
@@ -12,6 +13,11 @@ export default [
 		.isAlphanumeric()
 		.withMessage(localize('auth.invite.code.format_invalid'))
 		.bail()
-		.custom((value) => !!invitesService.validate(value))
+		.custom((value, { req }) => {
+			const invite = invitesService.getByCode(value);
+			return invite != null && invite.isSystemInvite()
+				? hasPermission(req.account.role, Permission.MANAGE_SYSTEM_INVITES)
+				: (req.account.id = invite.account.id);
+		})
 		.withMessage(localize('auth.invite.code.invalid')),
 ];
--- src/validation/validators/dashboard/invites/invite.share.validators.js
+++ src/validation/validators/dashboard/invites/invite.share.validators.js
@@ -1,6 +1,7 @@
 import { param } from 'express-validator';
 import localize from '../../../localize.js';
 import invitesService from '../../../../services/invites.service.js';
+import { hasPermission, Permission } from '../../../../models/roles.js';
 
 export default [
 	param('invite')
@@ -11,6 +12,11 @@ export default [
 		.isAlphanumeric()
 		.withMessage(localize('invite.param.format_invalid'))
 		.bail()
-		.custom((value) => !!invitesService.validate(value))
+		.custom((value, { req }) => {
+			const invite = invitesService.getByCode(value);
+			return invite != null && invite.isSystemInvite()
+				? hasPermission(req.account.role, Permission.MANAGE_SYSTEM_INVITES)
+				: (req.account.id = invite.account.id);
+		})
 		.withMessage(localize('invite.param.invalid')),
 ];
