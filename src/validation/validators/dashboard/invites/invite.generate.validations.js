9fb222a4bb83401a2100d7beabbc66cec5d1a088
pasewalck <pasewalck@posteo.net>
2026-04-30 16:30:44 +0200

    feat: escape label and invite mode field and make optional

--- src/validation/validators/dashboard/invites/invite.generate.validations.js
+++ src/validation/validators/dashboard/invites/invite.generate.validations.js
@@ -8,7 +8,7 @@ export default [
 		.isInt({ gt: 0, max: 1000 })
 		.withMessage(localize('validation.count.range', 1000))
 		.toInt(),
-	body('inviteMode'),
-	body('label'),
+	body('inviteMode').optional({ checkFalsy: true }).escape(),
+	body('label').optional({ checkFalsy: true }).escape(),
 	body('date').optional({ checkFalsy: true }).isDate().withMessage(localize('validation.date.invalid')).toDate(),
 ];
